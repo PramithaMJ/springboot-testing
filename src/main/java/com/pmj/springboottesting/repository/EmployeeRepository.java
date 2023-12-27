6439d2664756e5105c184877a55f578300cb237d
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-12-27 23:26:09 +0530

    Junit test for custom query using JPQL with index

--- src/main/java/com/pmj/springboottesting/repository/EmployeeRepository.java
+++ src/main/java/com/pmj/springboottesting/repository/EmployeeRepository.java
@@ -2,10 +2,15 @@ package com.pmj.springboottesting.repository;
 
 import com.pmj.springboottesting.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 //Dao
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     Optional<Employee> findByEmail(String email);
+
+    //define custom query using JPQL with index params
+    @Query("select e from Employee e where e.firstName =?1 and e.lastName = ?2")
+    Employee findByJPQL(String firstName,String lastName);
 }
--- src/test/java/com/pmj/springboottesting/repositry/EmployeeRepositoryTests.java
+++ src/test/java/com/pmj/springboottesting/repositry/EmployeeRepositoryTests.java
@@ -1,7 +1,11 @@
 package com.pmj.springboottesting.repositry;
+
 import com.pmj.springboottesting.model.Employee;
 import com.pmj.springboottesting.repository.EmployeeRepository;
+
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +22,7 @@ public class EmployeeRepositoryTests {
     // Junit test for save employee operation
     //@DisplayName("Junit test for save employee operation")
     @Test
-    public void givenEmployeeObject_whenSave_thenReturnSavedEmployee(){
+    public void givenEmployeeObject_whenSave_thenReturnSavedEmployee() {
 
         //given - precondition or setup
         Employee employee = Employee.builder()
@@ -40,7 +44,7 @@ public class EmployeeRepositoryTests {
     //Junit test for get all the employees operation
     @DisplayName("Junit test for get all the employees operation")
     @Test
-    public void givenEmployeesList_whenFindAll_thenEmployeesList(){
+    public void givenEmployeesList_whenFindAll_thenEmployeesList() {
         // given recondition or setup
         Employee employee = Employee.builder()
                 .firstName("Pramitha")
@@ -69,7 +73,7 @@ public class EmployeeRepositoryTests {
     //Junit test for get employee by id operation
     @DisplayName("Junit test for get employee by id operation")
     @Test
-    public void givenEmployeeObject_whenFindById_thenReturnEmployeeObject(){
+    public void givenEmployeeObject_whenFindById_thenReturnEmployeeObject() {
         // given recondition or setup
         Employee employee = Employee.builder()
                 .firstName("Pramitha")
@@ -91,7 +95,7 @@ public class EmployeeRepositoryTests {
     //Junit test for get employee by email operation
     @DisplayName("Junit test for get employee by email operation")
     @Test
-    public void givenEmployeeEmail_whenFindByEmail_thenReturnEmployeeObject(){
+    public void givenEmployeeEmail_whenFindByEmail_thenReturnEmployeeObject() {
         // given recondition or setup
         Employee employee = Employee.builder()
                 .firstName("Pramitha")
@@ -110,27 +114,27 @@ public class EmployeeRepositoryTests {
 
     //Junit test for update employee object
     @DisplayName("Junit test for update employee object")
-     @Test
-     public void givenEmployeeObject_whenUpdateEmployee_thenReturnUpdatedEmployee() {
-         // given recondition or setup
-         Employee employee = Employee.builder()
-                 .firstName("Pramitha")
-                 .lastName("Jayasooriya")
-                 .email("lpramithamj@gmail.com")
-                 .build();
-         employeeRepository.save(employee);
+    @Test
+    public void givenEmployeeObject_whenUpdateEmployee_thenReturnUpdatedEmployee() {
+        // given recondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Pramitha")
+                .lastName("Jayasooriya")
+                .email("lpramithamj@gmail.com")
+                .build();
+        employeeRepository.save(employee);
 
-         //when - action or the behavior that we are going to test
-         Employee savedEmployee = employeeRepository.findById(employee.getId()).get();
-         savedEmployee.setEmail("mihiranga@gmail.com");
-         savedEmployee.setFirstName("Mihiranga");
-         Employee updatedEmployee = employeeRepository.save(savedEmployee);
+        //when - action or the behavior that we are going to test
+        Employee savedEmployee = employeeRepository.findById(employee.getId()).get();
+        savedEmployee.setEmail("mihiranga@gmail.com");
+        savedEmployee.setFirstName("Mihiranga");
+        Employee updatedEmployee = employeeRepository.save(savedEmployee);
 
-         //then - verify the output
-         assertThat(updatedEmployee.getEmail()).isEqualTo("mihiranga@gmail.com");
-         assertThat(updatedEmployee.getFirstName()).isEqualTo("Mihiranga");
+        //then - verify the output
+        assertThat(updatedEmployee.getEmail()).isEqualTo("mihiranga@gmail.com");
+        assertThat(updatedEmployee.getFirstName()).isEqualTo("Mihiranga");
 
-     }
+    }
 
     //Junit test for delete employee operation
     @DisplayName("Junit test for delete employee operation")
@@ -145,10 +149,32 @@ public class EmployeeRepositoryTests {
 
         //when - action or the behavior that we are going to test
         employeeRepository.deleteById(employee.getId());
-        Optional<Employee>employeeOptional = employeeRepository.findById(employee.getId());
+        Optional<Employee> employeeOptional = employeeRepository.findById(employee.getId());
 
         //then - verify the output
         assertThat(employeeOptional).isEmpty();
+    }
+
+    //Junit test for custom query using JPQL with index
+    @DisplayName("Junit test for custom query using JPQL with index")
+    @Test
+    public void givenFirstNameAndLastName_whenFindByJPQL_thenReturnEmployeeObject() {
+        // given recondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Pramitha")
+                .lastName("Jayasooriya")
+                .email("lpramithamj@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+        String firstName = "Pramitha";
+        String lastName = "Jayasooriya";
+
+        //when - action or the behavior that we are going to test
+        Employee savedEmployee = employeeRepository.findByJPQL(firstName,lastName);
+
+        //then - verify the output
+        assertThat(savedEmployee).isNotNull();
+
     }
 
 }
